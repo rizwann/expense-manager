8835fc9b533e926d0a727b44063ef6bfcb6c0b82
Rizwan Kabir <rizwankabirsizan@gmail.com>
2024-09-03 00:30:31 +0200

    prettify

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
-import { ReactNode, useEffect, useState } from "react";
-import BarChartBox from "../../components/BarChartBox";
-import BigChart from "../../components/BigChart";
-import ChartBox from "../../components/ChartBox";
-import JoinHouse from "../../components/JoinHouse";
-import PieChartBox from "../../components/PieChartBox";
-import TopBox from "../../components/TopBox";
-import { useAuth } from "../../hooks/useAuth";
+import { ReactNode, useEffect, useState } from "react"
+import BarChartBox from "../../components/BarChartBox"
+import BigChart from "../../components/BigChart"
+import ChartBox from "../../components/ChartBox"
+import JoinHouse from "../../components/JoinHouse"
+import PieChartBox from "../../components/PieChartBox"
+import TopBox from "../../components/TopBox"
+import { useAuth } from "../../hooks/useAuth"
 import {
   barChartBoxAllUser,
   barChartBoxUserExpenseLastSixMonths,
@@ -13,24 +13,30 @@ import {
   chartBoxHouseExpense,
   chartBoxStoreExpense,
   chartBoxUserExpense,
-} from "../../menu-item";
-import "./home.scss";
-import { config } from "../../utils/config";
-import Button from "../components/Button";
-import { CircularProgress, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
-import AddHouse from "../../components/AddHouse";
-import axios from "axios";
-import { House } from "../../types";
-import { motion } from "framer-motion";
+} from "../../menu-item"
+import "./home.scss"
+import { config } from "../../utils/config"
+import Button from "../components/Button"
 import {
-  Home as HomeIcon
-} from "@mui/icons-material"
-type Props = {};
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material"
+import AddHouse from "../../components/AddHouse"
+import axios from "axios"
+import { House } from "../../types"
+import { motion } from "framer-motion"
+import { Home as HomeIcon } from "@mui/icons-material"
+type Props = {}
 
 const Home = (props: Props) => {
   const [joinHouseModal, setJoinHouseModal] = useState(false)
   const [createHouseModal, setCreateHouseModal] = useState(false)
-  const { user, selectedHouse, setRefresh } = useAuth();
+  const { user, selectedHouse, setRefresh } = useAuth()
   const token = localStorage.getItem("token")
   const [loading, setLoading] = useState(true)
   const [houses, setHouses] = useState<House[]>([])
@@ -56,7 +62,7 @@ const Home = (props: Props) => {
       setLoading(false)
     }
   }
-  console.log(user, "user");
+  console.log(user, "user")
   useEffect(() => {
     fetchHouses()
   }, [selectedHouseLocal])
@@ -78,62 +84,78 @@ const Home = (props: Props) => {
   const selectedHouseData = houses?.find(
     (house) => house.code === selectedHouseLocal
   )
-  if(!user) return null;
+  if (!user) return null
   if (!(user.houseCodes.length > 0)) {
-    return(
-      <div style={{
-        display: "flex",
-        flexDirection: "column",  
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}>
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
         <div className="text-center">
-        <Typography variant="h5" style={{
-          marginBottom: "1rem",
-        }
-        }>You're currently not associated with any house. </Typography>
-        <Typography variant="h5">Please join or create a house to get started.</Typography>
+          <Typography
+            variant="h5"
+            style={{
+              marginBottom: "1rem",
+            }}
+          >
+            You're currently not associated with any house.{" "}
+          </Typography>
+          <Typography variant="h5">
+            Please join or create a house to get started.
+          </Typography>
         </div>
-          <div className="flex flex-col items-center justify-center gap-4 mt-4 sm:flex-row">
-          <Button text="Join a House" size='lg' onClick={() => setJoinHouseModal(true)}/>
-          <Button text="Create a House" size='lg' onClick={() => setCreateHouseModal(true)}/>
-          </div>
+        <div className="flex flex-col items-center justify-center gap-4 mt-4 sm:flex-row">
+          <Button
+            text="Join a House"
+            size="lg"
+            onClick={() => setJoinHouseModal(true)}
+          />
+          <Button
+            text="Create a House"
+            size="lg"
+            onClick={() => setCreateHouseModal(true)}
+          />
+        </div>
         {joinHouseModal && (
           <JoinHouse
             setModalOpen={setJoinHouseModal}
-            columns={config.houseFields.filter((field) => field.field === "code")}
+            columns={config.houseFields.filter(
+              (field) => field.field === "code"
+            )}
             setRefresh={setRefresh}
           />
-        )
-  } 
-  {
-    createHouseModal && (
-      <AddHouse
-        setModalOpen={setCreateHouseModal}
-        columns={config.houseFields}
-        setRefresh={setRefresh}
-      />
-    )
-  }
+        )}
+        {createHouseModal && (
+          <AddHouse
+            setModalOpen={setCreateHouseModal}
+            columns={config.houseFields}
+            setRefresh={setRefresh}
+          />
+        )}
       </div>
     )
   }
-  console.log("housesheda", houses)
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",  
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-              <motion.div
-          className="w-full max-w-xs m-3"
-          initial={{ y: -50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-        >
-                <FormControl variant="outlined" className="w-full max-w-xs mb-6">
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <motion.div
+        className="w-full max-w-xs m-3"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+      >
+        <FormControl variant="outlined" className="w-full max-w-xs mb-6">
           <InputLabel id="select-house-label">Select House</InputLabel>
           <Select
             labelId="select-house-label"
@@ -142,52 +164,99 @@ const Home = (props: Props) => {
             label="Select House"
             className="text-white bg-gray-800"
             renderValue={(selected) => {
-              const selectedHouse = houses.find(house => house.code === selected);
-              return selectedHouse ? selectedHouse.description : '';
+              const selectedHouse = houses.find(
+                (house) => house.code === selected
+              )
+              return selectedHouse ? selectedHouse.description : ""
             }}
           >
             {houses.map((house) => (
               <MenuItem key={house.code} value={house.code}>
-               <HomeIcon className="mr-2 text-gray-500" />
+                <HomeIcon className="mr-2 text-gray-500" />
                 {house.description}
               </MenuItem>
             ))}
           </Select>
         </FormControl>
+      </motion.div>
+      <div className="home">
+        <div className="box box1">
+          {selectedHouseData && (
+            <TopBox user={user} houseCode={selectedHouseData?.code} />
+          )}
+        </div>
+        <div className="box box2">
+          {selectedHouseData && (
+            <ChartBox
+              {...chartBoxUserExpense}
+              user={user}
+              type="weeklyUser"
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
+        <div className="box box3">
+          {selectedHouseData && (
+            <ChartBox
+              {...chartBoxHouseExpense}
+              user={user}
+              type="houseExpenses"
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
+        <div className="box box4">
+          {selectedHouseData && (
+            <PieChartBox user={user} selectedHouse={selectedHouseData} />
+          )}
+        </div>
+        <div className="box box5">
+          {selectedHouseData && (
+            <ChartBox
+              {...chartBoxConversion}
+              user={user}
+              type="popularStore"
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
+        <div className="box box6">
+          {selectedHouseData && (
+            <ChartBox
+              {...chartBoxStoreExpense}
+              user={user}
+              type="popularCategory"
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
+        <div className="box box7">
+          {selectedHouseData && <BigChart selectedHouse={selectedHouseData} />}
+        </div>
+        <div className="box box8">
+          {selectedHouseData && (
+            <BarChartBox
+              {...barChartBoxUserExpenseLastSixMonths}
+              user={user}
+              type={"userSixMonths"}
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
 
-        </motion.div>
-    <div className="home">
-      <div className="box box1">
-        {selectedHouseData && <TopBox user={user} houseCode={selectedHouseData?.code} />}
-      </div>
-      <div className="box box2">
-        {selectedHouseData && <ChartBox {...chartBoxUserExpense} user={user} type="weeklyUser" selectedHouse={selectedHouseData} />}
-      </div>
-      <div className="box box3">
-        {selectedHouseData && <ChartBox {...chartBoxHouseExpense} user={user} type="houseExpenses" selectedHouse={selectedHouseData}  />}
-      </div>
-      <div className="box box4">
-        {selectedHouseData && <PieChartBox user={user} selectedHouse={selectedHouseData} />}
-      </div>
-      <div className="box box5">
-        {selectedHouseData && <ChartBox {...chartBoxConversion} user={user} type="popularStore" selectedHouse={selectedHouseData} />}
-      </div>
-      <div className="box box6">
-        {selectedHouseData && <ChartBox {...chartBoxStoreExpense} user={user} type="popularCategory" selectedHouse={selectedHouseData}  />}
-      </div>
-      <div className="box box7">
-        {selectedHouseData && <BigChart selectedHouse={selectedHouseData} />}
-      </div>
-      <div className="box box8">
-        {selectedHouseData && <BarChartBox {...barChartBoxUserExpenseLastSixMonths} user={user} type={"userSixMonths"} selectedHouse={selectedHouseData} />}
-      </div>
-
-      <div className="box box9">
-        {selectedHouseData && <BarChartBox {...barChartBoxAllUser} user={user} type={"contribution"} selectedHouse={selectedHouseData} />}
+        <div className="box box9">
+          {selectedHouseData && (
+            <BarChartBox
+              {...barChartBoxAllUser}
+              user={user}
+              type={"contribution"}
+              selectedHouse={selectedHouseData}
+            />
+          )}
+        </div>
       </div>
     </div>
-    </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
